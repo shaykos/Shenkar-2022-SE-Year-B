ba8986a3c0af4ccc4e96ed7fda5c18a0c5ce999b
shaykos <shaykos@gmail.com>
2022-11-29 16:57:21 +0200

    create your first react app

---
+++ intro-to-react/src/main.jsx
@@ -0,0 +1,14 @@
+//import from node_modules
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+//import from SRC folder
+import App from './App'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
