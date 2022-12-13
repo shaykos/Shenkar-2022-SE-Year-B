e5abdf6b1661464ff5cc7c990626f6f81457a286
shaykos <shaykos@gmail.com>
2022-12-13 17:18:55 +0200

    Hanukkah Example

---
+++ welcome to react/src/App.jsx
@@ -0,0 +1,40 @@
+import Header from "./components/Header"
+
+export default function App() {
+
+    return (
+        <>
+            <Header />
+            <div>
+                <h1>Hiiiii</h1>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur
+                    adipisicing elit. Quia, quae quos
+                    fugiat atque provident nesciunt eius
+                    amet, explicabo iusto, non consequuntur
+                    aliquid culpa magnam blanditiis
+                    quibusdam veniam laborum nobis numquam
+                    ullam velit vel cumque error omnis?
+                    Harum aperiam sint expedita dolorum
+                    commodi libero, incidunt, dolor nulla
+                    quisquam adipisci natus accusamus.
+                </p>
+                <img width="400" src="https://th.bing.com/th/id/R.670882977b4c746bb763737d266ffbb8?rik=jT%2bnOPdcsFh7fw&riu=http%3a%2f%2fs1.thingpic.com%2fimages%2ftR%2f3gfyokqj42bL5wmNhXyD8ote.jpeg&ehk=kVYffYk1PicmLvLss1OpzZCFuXwEv9Z%2bRicE5BAtGe8%3d&risl=&pid=ImgRaw&r=0" alt="" />
+                <Header />
+            </div>
+            <div>
+            <Header />
+                <h2>kuku</h2>
+                <Header />
+            </div>
+            <Header />
+            <Header />
+            <Header />
+            <Header />
+            <Header />
+            <Header />
+        </>
+    )
+
+}
+
---
+++ welcome to react/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+        <header className='app-header'>
+            <nav>
+                <ul>
+                    <li>item 1</li>
+                    <li>item 2</li>
+                    <li>item 3</li>
+                </ul>
+            </nav>
+        </header>
+    </>
+  )
+}
