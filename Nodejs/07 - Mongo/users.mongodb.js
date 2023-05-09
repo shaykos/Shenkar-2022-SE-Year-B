d3521cb0dacc778765d0f3e4a6d04676a5c498d0
shaykos <shaykos@gmail.com>
2023-05-09 11:48:41 +0300

    Update users.mongodb.js

--- Nodejs/07 - Mongo/users.mongodb.js
+++ Nodejs/07 - Mongo/users.mongodb.js
@@ -21,7 +21,29 @@ use(database);
 //     { name: "lilah", age: 25, status: "A" }
 // ])
 
-db.users.deleteMany({status:{$exists: false}});
-db.users.deleteMany({age:{$gte:25}});
-db.users.countDocuments();
+// db.users.deleteMany({status:{$exists: false}});
+// db.users.deleteMany({age:{$gte:25}});
+// db.users.countDocuments();
 
+//name עדכן את כל המסמכים שיש להם את התכונה 
+// עם הערך 70 age ותוסיף להם את התכונה 
+// db.users2.updateMany({ name: { $exists: true } }, {
+//     $set: {
+//         age: 70
+//     }
+// });
+
+// db.createCollection('a4');
+// db.a4.insertMany([
+//     {_id:1, arr:[1,2,3,4]},
+//     {_id:2, arr:[1,3,5,7]},
+//     {_id:3, arr:[3,5,4,7]},
+//     {_id:4, arr:[10,13,4,7]}
+// ])
+
+db.a4.updateMany({}, {$pull:{arr:{$gte:7}}})
+//מחיקת האיבר האחרון
+db.a4.updateOne({_id: 1}, {$pop:{arr:1}})
+
+//מחיקת האיבר הראשון 
+db.a4.updateOne({_id: 2}, {$pop:{arr:-1}})
